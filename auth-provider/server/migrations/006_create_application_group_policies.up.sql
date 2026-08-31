6a37c71136b2913325f67145a60395714f1827f1
SlackingSlothh <zkyrbbn@gmail.com>
2026-08-31 09:52:42 +0700

    fix: change table application_group_policies application_id from referencing client id to id

--- auth-provider/server/migrations/006_create_application_group_policies.up.sql
+++ auth-provider/server/migrations/006_create_application_group_policies.up.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE IF NOT EXISTS application_group_policies (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    application_id VARCHAR NOT NULL REFERENCES applications(client_id),
+    application_id UUID NOT NULL REFERENCES applications(id),
     group_id UUID NOT NULL REFERENCES groups(id),
     effect VARCHAR NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
